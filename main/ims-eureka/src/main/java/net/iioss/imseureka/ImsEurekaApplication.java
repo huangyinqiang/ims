18fa438db82779aa80cd5312e33bc9907119a2e1
huangyinqiang <13227718060@163.com>
2018-10-10 01:46:11 +0800

    新建ims-eureka项目

---
+++ main/ims-eureka/src/main/java/net/iioss/imseureka/ImsEurekaApplication.java
@@ -0,0 +1,12 @@
+package net.iioss.imseureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImsEurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ImsEurekaApplication.class, args);
+    }
+}
